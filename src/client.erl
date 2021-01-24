a5d4142ff455cca62c9b95b2d69dc673acf2e801
fbraberg <fillebraberg@gmail.com>
2021-01-24 13:19:34 +0100

    Add: added shell functions for client.erl

--- src/client.erl
+++ src/client.erl
@@ -5,6 +5,7 @@
 start_client(Name) ->
     spawn (fun() -> client_loop(#client{pid = self(), name = Name, status = active }) end).
 
+
 client_loop(Client) ->
     receive
         {ReqType, Data}         -> spawn (fun() -> handle_req(ServerPid, State, ReqType, Data) end);
@@ -12,10 +13,23 @@ client_loop(Client) ->
     end,
     client_loop(Client).
 
-
+% ========The following functions are for communicating with the server========
+% Send request to server
 send_request(ServerPid, Req, Data) ->
     ServerPid ! {Req, Data}.
 
+% Attempt to connect to server
+connect(ServerPid, ) -> 0.
+% =============================================================================
+
+
+% =======The following functions are for changing the local client state=======
+% Change your activitity status
+change_status() -> 0.
+
+% Check with the server if name is available, THEN change it.
+change_name() -> 0.
+% =============================================================================
 
 % An error has occured
 handle_err(Var) ->
